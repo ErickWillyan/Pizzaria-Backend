e1ee4ec5da75f01136a7cd8ff7a44b498ac9a5d2
Erick Willyan <erickwillyan2105@gmail.com>
2025-01-20 22:21:38 -0300

    add prisma and create migrations

---
+++ src/controllers/user/createUserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, response } from "express";
+
+class CreateUserController {
+  async handle(req: Request, res: Response) {
+    return res.json({ ok: true });
+  }
+}
+
+export { CreateUserController };
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors";
+
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
-app.use("/", router);
+app.use(cors());
 
-app.listen(3333, () => console.log("Servidor Rodando na porta 3333"));
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
+
+app.listen(3000, () =>
+  console.log("Servidor Rodando na porta 3000, http://localhost:3000/ ")
+);
