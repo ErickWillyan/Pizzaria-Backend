0c855dc02700de30737e2519da8ed2d6448faf87
Erick Willyan <erickwillyan2105@gmail.com>
2025-01-22 20:52:36 -0300

    feat/ detailUser

--- src/controllers/user/AuthUserController.ts
+++ src/controllers/user/AuthUserController.ts
@@ -4,6 +4,7 @@ import { AuthUserService } from "../../services/user/AuthUserService";
 class AuthUserController {
   async handle(req: Request, res: Response) {
     const { email, password } = req.body;
+
     const authUserService = new AuthUserService();
 
     const auth = await authUserService.execute({ email, password });
--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,8 +4,9 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 class DetailUserController {
   async handle(req: Request, res: Response) {
     const detailUserService = new DetailUserService();
+    const user_id = req.user_id;
 
-    const user = await detailUserService.execute();
+    const user = await detailUserService.execute(user_id);
 
     return res.json(user);
   }
